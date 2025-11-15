28ee729752f4680c50212e848557b8e3a76e8327
Nolaj <davidjalon@protonmail.com>
2025-11-15 20:24:05 +0100

    arreglado el inicio en modooscuro o monodia

--- app/src/main/java/com/mushi/notasapp/MainActivity.kt
+++ app/src/main/java/com/mushi/notasapp/MainActivity.kt
@@ -4,13 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import com.mushi.notasapp.BlocNotas.NotesActivity
+import com.mushi.notasapp.ajustes.AjustesViewModel
+import com.mushi.notasapp.ajustes.AjustesViewModelFactory
 import com.mushi.notasapp.data.database.AppDatabase
 import com.mushi.notasapp.data.database.entities.User
+import com.mushi.notasapp.data.local.SettingsManager
 import com.mushi.notasapp.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
 
@@ -20,8 +25,22 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var database: AppDatabase
 
+    private val ajustesViewModel: AjustesViewModel by viewModels {
+        AjustesViewModelFactory(SettingsManager(this))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        ajustesViewModel.isDarkModeEnabled.observe(this) { isEnabled ->
+            val mode = if (isEnabled) {
+                AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                AppCompatDelegate.MODE_NIGHT_NO
+            }
+            AppCompatDelegate.setDefaultNightMode(mode)
+        }
+
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
